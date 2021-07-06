4082863a3531bb095b810fe29b0f84d12597ccfa
Arkadiy Grigoryevich Novitskiy <ag.novitskiy@icloud.com>
2021-07-06 21:38:40 +0200

    Initial Commit

---
+++ Elysabettian/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Elysabettian
+//
+//  Created by Simone Rolando on 06/07/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
