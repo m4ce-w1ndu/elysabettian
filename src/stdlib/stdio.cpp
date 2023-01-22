cc712641f55835f35260fb22adee15418240ec85
arkgrv <ag.novitskiy@icloud.com>
2023-01-22 03:15:50 +0100

    Moved stdlib to fmt::print

--- src/stdlib/stdio.cpp
+++ src/stdlib/stdio.cpp
@@ -27,7 +27,6 @@ namespace stdlib {
 		return str == "true" || str == "false";
 	}
 
-
 	libstdio::libstdio()
 	: library_t({
 		// Console I/O
@@ -35,9 +34,9 @@ namespace stdlib {
 			if (argc > 0 && argc < 2) {
 				try {
 					const auto param = std::get<std::string>(*args);
-					std::cout << param;
+					fmt::print("{}", param);
 				} catch (std::bad_variant_access&) {
-					std::cerr << "Error: expected type is string." << std::endl;
+					fmt::print(stderr, "Error: expected type is string.\n");
 					return std::monostate();
 				}
 			}
@@ -59,7 +58,7 @@ namespace stdlib {
 
 		{ "fopen", [](int argc, std::vector<value_t>::iterator args) -> value_t {
 			if (argc < 2 || argc > 2) {
-				fprintf(stderr, "fopen(filename, mode) expects 2 parameters. Got %d.\n", argc);
+				fmt::print(stderr, "fopen(filename, mode) expects 2 parameters. Got {}.\n", argc);
 				return std::monostate();
 			}
 			try {
@@ -69,31 +68,31 @@ namespace stdlib {
 				try {
 					auto file = std::make_shared<file_obj>(name, mode);
 					if (!file->is_open()) {
-						fprintf(stderr, "Error: file %s is not open.\n", file->path.c_str());
+						fmt::print(stderr, "Error: file {} is not open.\n", file->path.c_str());
 						return std::monostate();
 					}
 					return file;
 				}
 				catch (std::out_of_range&) {
-					std::cerr << "Error: invalid file mode." << std::endl;
+					fmt::print(stderr, "Error: invalid file mode.");
 					return std::monostate();
 				}
 			}
 			catch (std::bad_variant_access&) {
-				std::cerr << "Error: expected types are {string} {string}" << std::endl;
+				fmt::print(stderr, "Error: expected types are {string} {string}\n");
 				return std::monostate();
 			}
 		}},
 
 		{ "fclose", [](int argc, std::vector<value_t>::iterator args) -> value_t {
 			if (argc < 1 || argc > 1) {
-				fprintf(stderr, "fclose(filestream) expects 1 parameter. Got %d.\n", argc);
+				fmt::print(stderr, "fclose(filestream) expects 1 parameter. Got {}.\n", argc);
 				return std::monostate();
 			}
 			try {
 				auto file_ptr = std::get<file_t>(*args);
 				if (!file_ptr->is_open()) {
-					fprintf(stderr, "Error: file %s is not open.\n", file_ptr->path.c_str());
+					fmt::print(stderr, "Error: file {} is not open.\n", file_ptr->path.c_str());
 					return std::monostate();
 				}
 
@@ -101,59 +100,59 @@ namespace stdlib {
 				return file_ptr;
 
 			} catch (std::bad_variant_access&) {
-				std::cerr << "Error: expected type is {file}" << std::endl;
+				fmt::print(stderr, "Error: expected type is {file}\n");
 				return std::monostate();
 			}
 		}},
 
 		{ "fflush", [](int argc, std::vector<value_t>::iterator args) -> value_t {
 			if (argc < 1 || argc > 1) {
-				fprintf(stderr, "fflush(filestream) expects 1 parameter. Got %d.\n", argc);
+				fmt::print(stderr, "fflush(filestream) expects 1 parameter. Got {}.\n", argc);
 				return std::monostate();
 			}
 			try {
 				auto file_ptr = std::get<file_t>(*args);
 				if (!file_ptr->is_open()) {
-					fprintf(stderr, "Error: file %s is not open.\n", file_ptr->path.c_str());
+					fmt::print(stderr, "Error: file {} is not open.\n", file_ptr->path.c_str());
 					return std::monostate();
 				}
 
 				file_ptr->flush();
 				return file_ptr;
 			} catch (std::bad_variant_access&) {
-				std::cerr << "Error: expected type is {file}" << std::endl;
+				fmt::print(stderr, "Error: expected type is {file}\n");
 				return std::monostate();
 			}
 		}},
 
 		{ "fread", [](int argc, std::vector<value_t>::iterator args) -> value_t {
 			if (argc < 1 || argc > 1) {
-				fprintf(stderr, "fread(filestream) expects 1 parameter. Got %d.\n", argc);
+				fmt::print(stderr, "fread(filestream) expects 1 parameter. Got {}.\n", argc);
 				return std::monostate();
 			}
 			try {
 				auto file_ptr = std::get<file_t>(*args);
 				if (!file_ptr->is_open()) {
-					fprintf(stderr, "Error: file %s is not open.\n", file_ptr->path.c_str());
+					fmt::print(stderr, "Error: file {} is not open.\n", file_ptr->path.c_str());
 					return std::monostate();
 				}
 
 				return file_ptr->read_all();
 			} catch (std::bad_variant_access&) {
-				std::cerr << "Error: expected type is {file}" << std::endl;
+				fmt::print(stderr, "Error: expected type is {file}\n");
 				return std::monostate();
 			}
 		}},
 		
 		{ "fwrite", [](int argc, std::vector<value_t>::iterator args) -> value_t {
 			if (argc < 2 || argc > 2) {
-				fprintf(stderr, "fwrite(filestream, data) expects 2 parameters. Got %d.\n", argc);
+				fmt::print(stderr, "fwrite(filestream, data) expects 2 parameters. Got {}.\n", argc);
 				return std::monostate();
 			}
 			try {
 				auto file_ptr = std::get<file_t>(*args);
 				if (!file_ptr->is_open()) {
-					fprintf(stderr, "Error: file %s is not open.\n", file_ptr->path.c_str());
+					fmt::print(stderr, "Error: file {} is not open.\n", file_ptr->path.c_str());
 					return std::monostate();
 				}
 
@@ -161,7 +160,7 @@ namespace stdlib {
 				file_ptr->write_all(data);
 				return file_ptr;
 			} catch (std::bad_variant_access&) {
-				std::cerr << "Error: expected types are {file} {string}" << std::endl;
+				fmt::print(stderr, "Error: expected types are {file} {string}\n");
 				return std::monostate();
 			}
 		}}
