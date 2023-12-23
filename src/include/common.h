0fe481635a1822486d0eb8cf192471fc7f5fcf1f
M4ce W1ndu <m4ce.w1ndu@icloud.com>
2023-12-23 17:37:35 +0100

    Add documentation to common.h. Update version to 1.1.3

--- src/include/common.h
+++ src/include/common.h
@@ -1,9 +1,12 @@
 #ifndef ELY_RT_COMMON_H
 #define ELY_RT_COMMON_H
 
+// Standard C++ headers.
 #include <iostream>
 #include <vector>
 #include <utility>
+
+// fmt headers.
 #include <fmt/core.h>
 #include <fmt/printf.h>
 #include <fmt/format.h>
@@ -14,9 +17,19 @@
 #undef DEBUG_PRINT_CODE
 #undef DEBUG_TRACE_EXECUTION
 
+/**
+ * @brief Max number of UINT8 variables.
+*/
 constexpr auto UINT8_COUNT = UINT8_MAX + 1;
 
+/**
+ * @brief Current version of the language.
+*/
 constexpr auto VERSION = "1.1.2";
-constexpr auto VERSION_FULLNAME = "Elysabettian 1.1.2 Jupiter (JIT)";
+
+/**
+ * @brief Full name of the language's version.
+*/
+constexpr auto VERSION_FULLNAME = "Elysabettian 1.1.3 Jupiter (JIT)";
 
 #endif
