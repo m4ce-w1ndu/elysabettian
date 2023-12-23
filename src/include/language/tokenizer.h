d82a0ac117037c612fde8c5351a4b23afdc44bf4
M4ce W1ndu <m4ce.w1ndu@icloud.com>
2023-12-23 17:54:25 +0100

    Add documentation to tokenizer.h

--- src/include/language/tokenizer.h
+++ src/include/language/tokenizer.h
@@ -1,8 +1,12 @@
 #ifndef ELY_RT_TOKENIZER_H
 #define ELY_RT_TOKENIZER_H
 
+// Standard C++ headers.
 #include <string>
 
+/**
+ * @brief Describes the type of a Token.
+*/
 enum class TokenType {
     // Single-character tokens.
     OpenParen, CloseParen,
@@ -35,55 +39,157 @@ enum class TokenType {
     OpenSquare, CloseSquare,
 };
 
+/**
+ * @brief Describes a Token in the Elysabettian language.
+*/
 class Token {
+    /**
+     * @brief Type of this token.
+    */
     TokenType type;
+
+    /**
+     * @brief Text content of this token.
+    */
     std::string_view text;
+
+    /**
+     * @brief Position in lines of this token.
+    */
     int line;
     
 public:
+    /**
+     * @brief Create a new Token.
+     * @param type Type of this new token.
+     * @param text Text content of this new token.
+     * @param line Line number of this new token.
+    */
     Token(TokenType type, std::string_view text, int line):
         type(type), text(text), line(line) {};
     
+    /**
+     * @brief Get the type of this token.
+     * @return Type of this token.
+    */
     TokenType get_type() const { return type; };
+
+    /**
+     * @brief Get the text content of this token.
+     * @return Text of this token as std::string_view.
+    */
     std::string_view get_text() const { return text; };
+
+    /**
+     * @brief Get the line number of this token.
+     * @return Line number of this token.
+    */
     int get_line() const { return line; };
 };
 
+/**
+ * @brief Tokenizes the source code for this 
+*/
 class Tokenizer {
-    
-    const std::string::size_type start_default { 0u };
-    const std::string::size_type current_default { 0u };
-    const int line_default { 1u };
-
-    std::string source;
-    std::string::size_type start;
-    std::string::size_type current;
-    int line;
-    
-    bool is_at_end() const;
-    char advance();
-    char peek();
-    char peek_next();
-    bool match(char expected);
-    
-    Token make_token(TokenType type);
-    Token error_token(const std::string& message) const;
-    
-    void skip_whitespace();
-    TokenType check_keyword(size_t pos, size_t len, const std::string_view& rest, TokenType type) const;
-    TokenType identifier_type();
-    Token identifier();
-    Token number();
-    Token string_(const char open_char = '"');
-    
 public:
-    explicit Tokenizer(const std::string& source):
-        source{source},
-        start{start_default},
-        current{current_default},
-        line{line_default} {};
+    /**
+     * @brief Create a new Tokenizer.
+     * @param source Source code to tokenize.
+    */
+    explicit Tokenizer(const std::string& source)
+        : source(source), start(0u), current(0u), line(1) {}
     
+    /**
+     * @brief Scan the next token.
+     * @return Scanned token.
+    */
     Token scan_token();
+
+private:
+    /**
+     * @brief Source code.
+    */
+    std::string source;
+
+    /**
+     * @brief Start of the tokenizer.
+    */
+    std::string::size_type start;
+
+    /**
+     * @brief Current position of the tokenizer.
+    */
+    std::string::size_type current;
+
+    /**
+     * @brief Current line of the tokenizer.
+    */
+    int line;
+
+    /**
+     * @brief Check if the tokenizer reached end of source code.
+    */
+    bool is_at_end() const;
+
+    /**
+     * @brief Advance the tokenizer.
+    */
+    char advance();
+
+    /**
+     * @brief Read current character.
+    */
+    char peek();
+
+    /**
+     * @brief Read next character.
+    */
+    char peek_next();
+
+    /**
+     * @brief Match current character.
+    */
+    bool match(char expected);
+
+    /**
+     * @brief Make a new token.
+    */
+    Token make_token(TokenType type);
+
+    /**
+     * @brief Make an error token.
+    */
+    Token error_token(const std::string& message) const;
+
+    /**
+     * @brief Skip whitespaces in source code.
+    */
+    void skip_whitespace();
+
+    /**
+     * @brief Check for a keyword.
+    */
+    TokenType check_keyword(size_t pos, size_t len, const std::string_view& rest, TokenType type) const;
+
+    /**
+     * @brief Check the type of the indentifier.
+    */
+    TokenType identifier_type();
+
+    /**
+     * @brief Tokenize an identifier.
+    */
+    Token identifier();
+
+    /**
+     * @brief Tokenize a number.
+    */
+    Token number();
+
+    /**
+     * @brief Tokenize a string.
+    */
+    Token string_(const char open_char = '"');
 };
 
 #endif /* scanner_hpp */
