f1418c955ddfe20bdf28301d653b4d1e68320e0f
m4ce-w1ndu <m4ce.w1ndu@icloud.com>
2024-01-07 06:09:01 +0100

    Add new project. Add implementation of TokenType and Token

---
+++ Infrastructure/Language/Token.cs
@@ -0,0 +1,53 @@
+﻿namespace Infrastructure.Language
+{
+    /// <summary>
+    /// Describes the type of a Token.
+    /// </summary>
+    public enum TokenType
+    {
+        // Single-character tokens.
+        OpenParen, CloseParen,
+        OpenCurly, CloseCurly,
+        Comma, Dot, Minus, Plus,
+        Semicolon, Slash, Star,
+
+        // One or two character tokens.
+        Excl, ExclEqual,
+        Equal, EqualEqual,
+        Greater, GreaterEqual,
+        Less, LessEqual,
+
+        // Literals.
+        Identifier, String, Number,
+
+        // Keywords.
+        And, Class, Else, False,
+        Func, For, If, Null, Or,
+        Print, Return, Super, This,
+        True, Var, While,
+
+        Error,
+        Eof,
+
+        // Bitwise operations
+        BwAnd, BwOr, BwXor, BwNot,
+
+        // Array index notation
+        OpenSquare, CloseSquare,
+    }
+
+    /// <summary>
+    /// Describes a Token in the Elysabettian language.
+    /// </summary>
+    /// <param name="type">Type of this token.</param>
+    /// <param name="text">Text content of this token.</param>
+    /// <param name="line">Line number of this token.</param>
+    public struct Token(TokenType type, string text, int line)
+    {
+        public TokenType Type { get; set; } = type;
+
+        public string Text { get; set; } = text;
+
+        public int Line { get; set; } = line;
+    }
+}
